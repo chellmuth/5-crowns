b64141f311f9faec73faef8467df405672ec6fe4
Chris Hellmuth <chellmuth@gmail.com>
2020-01-12 12:26:03 -0800

    Cleanup

--- tests/test_find_sets.py
+++ tests/test_find_sets.py
@@ -8,100 +8,41 @@ H3 = Card(Suit.Hearts, Rank.Three)
 C3 = Card(Suit.Clubs, Rank.Three)
 S3 = Card(Suit.Spades, Rank.Three)
 
-class TestFindSets(unittest.TestCase):
-    def test_basic__find_three__no_wilds(self):
-        hand = [
-            H3,
-            C3,
-        ]
+class TestFindSets_size_three(unittest.TestCase):
+    def test_find_three__no_wilds(self):
+        hand = [H3, C3]
 
-        matches = find_sets(
-            S3,
-            hand,
-            0,
-            3
-        )
+        matches = find_sets(S3, hand, 0, 3)
 
         self.assertEqual(
             matches,
             [
-                Match(
-                    set([
-                        H3,
-                        C3,
-                        S3,
-                    ]),
-                    set(),
-                    0
-                )
+                Match(set([H3, C3, S3]), set(), 0)
             ]
         )
 
-    def test_basic__one_card_two_wilds(self):
-        hand = [
-            H3,
-            C3,
-        ]
+    def test_one_card_two_wilds(self):
+        hand = set([H3, C3])
 
-        matches = find_sets(
-            S3,
-            hand,
-            2,
-            3
-        )
+        matches = find_sets(S3, hand, 2, 3)
 
         self.assertEqual(
             matches,
             [
-                Match(
-                    set([
-                        S3,
-                    ]),
-                    set([
-                        H3,
-                        C3,
-                    ]),
-                    2
-                )
+                Match(set([S3]), set([H3, C3]), 2)
             ]
         )
 
-    def test_basic__two_options__one_wild(self):
-        hand = [
-            H3,
-            C3,
-        ]
+    def test_two_options__one_wild(self):
+        hand = set([H3, C3])
 
-        matches = find_sets(
-            S3,
-            hand,
-            1,
-            3
-        )
+        matches = find_sets(S3, hand, 1, 3)
 
         self.assertEqual(
             matches,
             [
-                Match(
-                    set([
-                        S3,
-                        H3,
-                    ]),
-                    set([
-                        C3,
-                    ]),
-                    1
-                ),
-                Match(
-                    set([
-                        S3,
-                        C3,
-                    ]),
-                    set([
-                        H3,
-                    ]),
-                    1
-                )
+                Match(set([S3, H3]), set([C3]), 1),
+                Match(set([S3, C3]), set([H3]), 1)
             ]
         )
         
