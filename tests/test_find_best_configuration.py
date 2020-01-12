73ebfdd324376350f5bcf530927f15157ae3df45
Chris Hellmuth <chellmuth@gmail.com>
2020-01-12 13:14:37 -0800

    Tougher test cases

--- tests/test_find_best_configuration.py
+++ tests/test_find_best_configuration.py
@@ -25,3 +25,16 @@ class TestBestConfiguration_size_four__sets__no_wilds(unittest.TestCase):
     def test_no_match(self):
         best = find_best_configuration(set([H3, C3, S4, C4]))
         self.assertEqual(best.score, 14)
+
+class TestBestConfiguration_two_sets__no_wilds(unittest.TestCase):
+    def test_two_sets_of_three(self):
+        best = find_best_configuration(set([H3, C3, S3, H4, C4, S4]))
+        self.assertEqual(best.score, 0)
+
+    def test_two_sets_of_different_sizes(self):
+        best = find_best_configuration(set([H3, C3, S3, D3, H4, C4, S4]))
+        self.assertEqual(best.score, 0)
+
+    def test_two_sets_of_different_sizes__with_leftovers(self):
+        best = find_best_configuration(set([H3, C3, S3, D3, H4, C4, S4, S5, C5]))
+        self.assertEqual(best.score, 10)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,8 +5,10 @@ C3 = Card(Suit.Clubs, Rank.Three)
 S3 = Card(Suit.Spades, Rank.Three)
 D3 = Card(Suit.Diamonds, Rank.Three)
 
-S4 = Card(Suit.Spades, Rank.Four)
+H4 = Card(Suit.Hearts, Rank.Four)
 C4 = Card(Suit.Clubs, Rank.Four)
+S4 = Card(Suit.Spades, Rank.Four)
+D4 = Card(Suit.Diamonds, Rank.Four)
 
 S5 = Card(Suit.Spades, Rank.Five)
 C5 = Card(Suit.Clubs, Rank.Five)
