bf1630e358dfc5ffa464811f2c2921482ded319d
Chris Hellmuth <chellmuth@gmail.com>
2020-01-12 16:07:58 -0800

    Show current configuration

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,8 @@ from crowns.cards import Rank, Suit
 from app import app
 
 def card_to_fragment(card):
+    if card == "*": return "*"
+
     ranks = {
         Rank.Three: "3",
         Rank.Four: "4",
@@ -38,10 +40,10 @@ def card_to_fragment(card):
 @app.route('/')
 def hello_world():
     hand_size = 7
-    wilds = 1
+    wilds = 2
     hand = dealer.deal(hand_size, hand_size - wilds)
 
-    current_score = scoring.find_best_configuration(hand, wilds).score
+    current_configuration = scoring.find_best_configuration(hand, wilds)
 
     simulated = simulation.simulate(hand, wilds)
 
@@ -69,7 +71,15 @@ def hello_world():
         ],
         "wild_score": simulated["wild_score"],
         "expected_score": simulated["expected_score"],
-        "current_score": current_score
+        "current_score": current_configuration.score,
+        "current_matches": [
+            [
+                card_to_fragment(card)
+                for card in match.cards
+            ] + [ "*" for _ in range(match.wilds) ]
+            for match in current_configuration.matches
+        ],
+        "current_hand": [ card_to_fragment(card) for card in current_configuration.matches[-1].hand ]
     }
 
     return render_template('index.html', title='Five Crowns: Hand Analyzer', game=game)
